55a4f5af4ed6ca203452081d5b454f19f42b6e4c
jianwei <developer@just4fun.sg>
2022-01-14 14:03:02 +0800

    no message

--- app/src/main/java/sg/just4fun/tgasdk/tga/global/Global.java
+++ app/src/main/java/sg/just4fun/tgasdk/tga/global/Global.java
@@ -2,9 +2,9 @@ package sg.just4fun.tgasdk.tga.global;
 
 public class Global {
 
-    public static final String YUMING="http://tgadev.just4fun.asia";//开发服
+  //  public static final String YUMING="http://tgadev.just4fun.asia";//开发服
 //      public static final String YUMING="https://data.just4fun.sg";//测试服
-//    public static final String YUMING="https://tga.just4fun.sg";//正式服
+    public static final String YUMING="https://tga.just4fun.sg";//正式服
 
 
       public static final String TEST_HOST_GOOGLE_PAY_URL_NEW=YUMING+"/sb/";
